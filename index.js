2fc60dc485cefdccbc14b41f3dd404ed67328786
LeviAckr <thech.nasa.dj@gmail.com>
2023-01-21 17:44:48 -0300

    ceira2

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const { Client, GatewayIntentBits, IntentsBitField,  ActivityType, Collection, Events, message, Partials,  SlashCommandBuilder} = require('discord.js')
+const { Client, GatewayIntentBits, IntentsBitField, ActivityType, Collection, Events, message, Partials, SlashCommandBuilder } = require('discord.js')
 const fs = require('fs')
 const path = require('node:path');
 const handler = require(`wax-command-handler`);
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        IntentsBitField.Flags.GuildMembers,
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		IntentsBitField.Flags.GuildMembers,
 		GatewayIntentBits.GuildMessageReactions
-    ],
-	partials:[
+	],
+	partials: [
 		Partials.Message,
 		Partials.GuildMember,
 		Partials.Reaction,
@@ -22,71 +22,71 @@ const client = new Client({
 const prefix = "["
 
 client.on("ready", () => {
-    
-//Logando com a pasta de comandos!
 
-client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] O comando ${filePath} ainda não possui o necessario para sua execusão.`);
-	}
+	// Logando com a pasta de comandos!
 
-    
-}
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
-
-	try {
-		await command.execute(client, interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-});
-
-//Inicio 
-
-    client.user.setPresence({
-        activities: [{ name: `🎧 Som na caixa Dj 🎤`, type: ActivityType.Playing}],
-        status: 'online',
-      });
-
-	  const levi = client.emojis.cache.get("1066189970919923782")
-	  const mikasa = client.emojis.cache.get("1066189968722120835")
-		client.on("messageCreate",  (message) =>{ 
-			if (message.mentions.has(client.user)) {
-			message.channel.send( (`${mikasa}`)  + "`Prontinho pra tocar sua musica`" +  (`${levi}`) );
-
-			
+	client.commands = new Collection();
+	const commandsPath = path.join(__dirname, 'commands');
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] O comando ${filePath} ainda não possui o necessario para sua execusão.`);
 		}
 
-		})
+
+	}
+	client.on(Events.InteractionCreate, async interaction => {
+		if (!interaction.isChatInputCommand()) return;
+
+		const command = interaction.client.commands.get(interaction.commandName);
+
+		if (!command) {
+			console.error(`No command matching ${interaction.commandName} was found.`);
+			return;
+		}
+
+		try {
+			await command.execute(client, interaction);
+		} catch (error) {
+			console.error(error);
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	});
+
+	//Inicio 
+
+	client.user.setPresence({
+		activities: [{ name: `🎧 Som na caixa Dj 🎤`, type: ActivityType.Playing }],
+		status: 'online',
+	});
+
+	const levi = client.emojis.cache.get("1066189970919923782")
+	const mikasa = client.emojis.cache.get("1066189968722120835")
+	client.on("messageCreate", (message) => {
+		if (message.mentions.has(client.user)) {
+			message.channel.send((`${mikasa}`) + "`Prontinho pra tocar sua musica`" + (`${levi}`));
 
 
-		//PING CMMD
-		
-		const um = client.emojis.cache.get("1066210454449442928")
-		client.on("messageCreate", (message) =>{
-			if(message.author.bot) return;
-		
-			if(message.content === prefix + "ping") message.channel.send ((`${um}`) + `**TATAKAE ** o ping é aproximadamente **${client.ws.ping}ms**` + (`${um}`));
-		})
-		
-  console.log("Pronto Para Tocar 🎼");
-  });
+		}
+
+	})
+
+
+	//PING CMMD
+
+	const um = client.emojis.cache.get("1066210454449442928")
+	client.on("messageCreate", (message) => {
+		if (message.author.bot) return;
+
+		if (message.content === prefix + "ping") message.channel.send((`${um}`) + `**TATAKAE ** o ping é aproximadamente **${client.ws.ping}ms**` + (`${um}`));
+	})
+
+	console.log("Pronto Para Tocar 🎼");
+});
 
 
 
